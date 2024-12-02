b7ee9d3b235363fa36dbbd4fc9dfcc2c57859c91
Christopher Wong <cwong8864@gmail.com>
2024-12-02 11:33:06 -0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import Examples.Ex2_Main;
+import Examples.Ex3_LawnMain;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Object Oriented Programming - Day 1 Block 2");
+
+//        Examples.Ex1_ArrayLists.run();
+//        Ex2_Main.run();
+
+        Ex3_LawnMain.run();
+    }
+
+}
