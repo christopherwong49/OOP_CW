3e7aedd552485a403c7887cfc07b596a5cd3804f
Christopher Wong <cwong8864@gmail.com>
2024-12-09 23:13:46 -0700

    12/09/2024

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,10 @@
-import Assignments.As1_Main;
-import Examples.Ex2_Main;
-import Examples.Ex3_LawnMain;
+import Assignments.As3_LeagueMain;
+
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-//        Examples.Ex1_ArrayLists.run();
-//        Ex2_Main.run();
-
-        As1_Main.run();
+        Assignments.As3_LeagueMain.run();
     }
 
 }
